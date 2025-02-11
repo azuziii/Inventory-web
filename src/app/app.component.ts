fc2df639e3058ef99c94b2bc4efa2b96603a2625
Azuzi <azuzii@proton.me>
2025-02-11 17:19:24 +0100

    feat: import header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { routes } from './app.routes';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SideNavComponent } from './shared/components/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideNavComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
