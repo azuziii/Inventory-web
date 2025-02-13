58fc2a23ee318ce2565a8ee5079a43eef9268c61
Azuzi <azuzii@proton.me>
2025-02-13 08:34:16 +0100

    feat: add dynamic title, and remove buttons

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,19 +1,41 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { Toolbar } from 'primeng/toolbar';
+import { filter, map, mergeMap, Observable } from 'rxjs';
 import { SideNavComponent } from '../side-nav/side-nav.component';
 
 @Component({
   selector: 'app-header',
-  imports: [Toolbar, ButtonModule, SideNavComponent],
+  imports: [Toolbar, ButtonModule, SideNavComponent, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  constructor(
+    protected readonly route: Router,
+    private readonly activatedRoute: ActivatedRoute,
+  ) {}
+
   items: MenuItem[] | undefined;
+  title$!: Observable<string>;
 
   ngOnInit() {
+    this.title$ = this.route.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((_) => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      mergeMap((route) => route.data),
+      map((data) => data['title']),
+    );
+
     this.items = [
       {
         label: 'Update',
