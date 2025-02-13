245f132b1b54c17be1830ef7e3f7ac3fd0deedd9
Azuzi <azuzii@proton.me>
2025-02-13 08:38:34 +0100

    feat: add  header to app component with some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { SideNavComponent } from './shared/components/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideNavComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
