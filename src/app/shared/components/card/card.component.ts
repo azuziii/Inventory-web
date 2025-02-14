391931ef4023a0ffe999f71c2a740a47243696e2
Azuzi <azuzii@proton.me>
2025-02-14 15:23:23 +0100

    feat: add klass input

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input() klass = '';
 }
