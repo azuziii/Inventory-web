629edda311e615cc27726a34766f8cd39e2f8e25
Azuzi <azuzii@proton.me>
2025-02-14 15:22:14 +0100

    feat: add getDirtyValues

--- src/app/shared/utils/get-dity-values.ts
+++ src/app/shared/utils/get-dity-values.ts
@@ -1,10 +1,24 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup } from '@angular/forms';
 
 export function getDirtyValues<T>(formGroup: FormGroup): T {
   const values: Record<string, any> = {};
   Object.keys(formGroup.controls).forEach((control) => {
-    if (formGroup.get(control)!.dirty) {
-      values[control] = formGroup.get(control)!.value;
+    const target = formGroup.get(control);
+    if (target instanceof FormArray && target.dirty) {
+      values[control] = ((target as FormArray).controls as FormGroup[])
+        .filter((c) => c.dirty)
+        .map((c) => {
+          const dirtyFields: Record<string, any> = {};
+          Object.keys(c.controls).forEach((k) => {
+            const arrayControlTarget = c.get(k);
+            if (arrayControlTarget?.dirty) {
+              dirtyFields[k] = arrayControlTarget.value;
+            }
+          });
+          return dirtyFields;
+        });
+    } else if (target!.dirty) {
+      values[control] = target!.value;
     }
   });
   return values as T;
